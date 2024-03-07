64867e29260046aa30bbf47531f649f01823d733
rvanasa <ryan.vandersmith@dfinity.org>
2024-03-07 14:55:05 -0700

    Add deploy mode dropdown

--- webview-ui/src/components/App.tsx
+++ webview-ui/src/components/App.tsx
@@ -1,7 +1,14 @@
-import { VSCodeLink } from '@vscode/webview-ui-toolkit/react';
+import {
+    VSCodeDivider,
+    VSCodeDropdown,
+    VSCodeOption,
+    VSCodePanels,
+    VSCodePanelTab,
+    VSCodePanelView,
+} from '@vscode/webview-ui-toolkit/react';
 import { useEventListener, useInterval } from 'usehooks-ts';
 import { postVSCodeMessage, useVSCodeState } from '../hooks/useVSCode';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import QRCode from 'react-qr-code';
 
 interface CanisterListItemProps {
@@ -21,8 +28,13 @@ function CanisterListItem({ text, link }: CanisterListItemProps) {
     );
 }
 
+type DeployMode = 'Local' | 'Playground' | 'ICP mainnet';
+
+const deployModes: DeployMode[] = ['Local', 'Playground', 'ICP mainnet'];
+
 function App() {
     const [vscodeState, setVSCodeState] = useVSCodeState();
+    const [deployMode, setDeployMode] = useState<DeployMode>('Local');
     const balanceRefreshIntervalSeconds = 5;
 
     useInterval(() => {
@@ -120,46 +132,70 @@ function App() {
     return (
         <main>
             <h2>Develop</h2>
+            <br />
 
-            <div>
-                <button onClick={publishCanisters}>
-                    Publish canisters on ICP mainnet
-                </button>
-            </div>
-
-            {!!vscodeState?.accountId && (
+            <label htmlFor="deploy-dropdown">Deployment mode:</label>
+            <br />
+            <VSCodeDropdown value={deployMode}>
+                {deployModes.map((mode) => (
+                    <VSCodeOption onSelect={() => setDeployMode(mode)}>
+                        {mode}
+                    </VSCodeOption>
+                ))}
+            </VSCodeDropdown>
+            <VSCodeDivider />
+            {deployMode === 'Local' && (
+                <button onClick={dfxDeploy}>Deploy locally</button>
+            )}
+            {deployMode === 'Playground' && (
+                // TODO
+                <button onClick={undefined}>Deploy to playground</button>
+            )}
+            {deployMode === 'ICP mainnet' && (
                 <>
-                    <br />
-                    <h2>QR Code</h2>
+                    <div>
+                        <button onClick={publishCanisters}>
+                            Deploy on ICP mainnet
+                        </button>
+                    </div>
 
-                    <p>
-                        Please transfer {vscodeState.requiredIcp ?? 420} ICP to{' '}
-                        {vscodeState.accountId}
-                    </p>
+                    {!!vscodeState?.accountId && (
+                        <>
+                            <br />
+                            <h2>QR Code</h2>
 
-                    <QRCode
-                        // TODO: maybe refactor to CSS
-                        style={{
-                            height: 'auto',
-                            background: 'white',
-                            padding: 10,
-                            maxWidth: '100%',
-                            width: '100%',
-                        }}
-                        size={256}
-                        viewBox="0 0 256 256"
-                        value={vscodeState.accountId}
-                    />
-                    <br />
-                    <h3>ICP balance: {vscodeState.icpBalance ?? 42}</h3>
-                    <h3>
-                        XDR (T cycles) balance: {vscodeState.xdrBalance ?? 42}
-                    </h3>
+                            <p>
+                                Please transfer {vscodeState.requiredIcp ?? 420}{' '}
+                                ICP to {vscodeState.accountId}
+                            </p>
+
+                            <QRCode
+                                // TODO: maybe refactor to CSS
+                                style={{
+                                    height: 'auto',
+                                    background: 'white',
+                                    padding: 10,
+                                    maxWidth: '100%',
+                                    width: '100%',
+                                }}
+                                size={256}
+                                viewBox="0 0 256 256"
+                                value={vscodeState.accountId}
+                            />
+                            <br />
+                            <h3>ICP balance: {vscodeState.icpBalance ?? 42}</h3>
+                            <h3>
+                                XDR (T cycles) balance:{' '}
+                                {vscodeState.xdrBalance ?? 42}
+                            </h3>
+                        </>
+                    )}
                 </>
             )}
 
             <br />
-            <h2>Canisters</h2>
+            <VSCodeDivider />
+            <br />
 
             <div>
                 {frontendCanisters.map(({ canister, id }, i) => (
